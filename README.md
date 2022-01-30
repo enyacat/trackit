b39a086d932be9f915c0db31bfb540a1cdbfafe2
Siyu Guo <enyacat@gmail.com>
2022-01-30 22:47:28 +1100

    setup heroku and update heroku url

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # An app to track your houseware
+https://trackhouseware.herokuapp.com/
 
+Register and login to build your own list by adding from the stock items or simply creating your own. A list that you can sort.
 # technology used:
     # ruby
     # sinatra
     # postgresql
     # bootstrap - to beautify buttons and a drop down menu
 
-# Note: all images are from animal crossing provided by ACNH API (http://acnhapi.com/)
+# Note: all images are from the Nintendo game animal crossing provided by ACNH API (http://acnhapi.com/)
 
 # brief app wireframe
 https://miro.com/app/board/uXjVOXb0zKg=/?invite_link_id=569813687456
 
-# app can be accessed here
-https://enyacat.github.io/trackit/
+# Github
+https://github.com/enyacat/trackit
 
 # Acknowledgement
-to world's smartest corgi
+to world's smartest corgi and DT
---
+++ db/data_processing.rb
@@ -0,0 +1,34 @@
+require 'json'
+require 'pry'
+file = File.read('houseware.json')
+data_hash=JSON.parse(file)
+require_relative "../models/houseware.rb"
+
+
+
+# conn = PG.connect(dbname: 'inventory')
+data_hash.each {|key, value|
+    i = 0
+    while i < value.length
+        name = key
+        if value[i]['variant'] == nil
+            variant = 0
+        else
+            variant = value[i]['variant']
+        end
+        image_url = value[i]['image_uri']
+        if value[i]['tag'] == nil
+            tag = 0
+        else
+            tag = value[i]['tag']
+        end
+        user_id = "2"
+        purchase_date = '2022-01-01'
+        quantity = 1
+        expiry_date = '9999-01-01'
+        create_item(name, variant, image_url, tag, user_id, purchase_date, quantity, expiry_date)
+        i += 1
+    end
+}
+
+
